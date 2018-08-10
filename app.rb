e4986657825d5d30f0b75c1ecce50fc7c2d158d0
Fabienne Rauber <fabienne.rauber@gmail.com>
2018-08-10 02:40:25 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -22,8 +22,7 @@ class App < Sinatra::Base
 
   get "/multiply/:num1/:num2" do
     @product = params[:num1].to_i * params[:num2].to_i
-    @num2 = params[:num2]
-    "#{num1+num2}"
+    "#{product}"
   end
 
 end
